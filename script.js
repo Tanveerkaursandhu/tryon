4fcbd606640dcf403cddfbcb5a5d3e4e1a18fdb4
Tanveerkaursandhu <sandhu20010001@gmail.com>
2025-11-13 10:30:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// const camera = document.getElementById('camera');
-// const outfit = document.getElementById('outfit');
+const camera = document.getElementById('camera');
+const outfit = document.getElementById('outfit');
 
-// async function startCamera() {
-//   try {
-//     const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
-//     camera.srcObject = stream;
-//   } catch (error) {
-//     console.error(error);
-//     alert("Please allow camera access to try the outfit!");
-//   }
-// }
+async function startCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+    camera.srcObject = stream;
+  } catch (error) {
+    console.error(error);
+    alert("Please allow camera access to try the outfit!");
+  }
+}
 
-// // Wait for user interaction (needed on iPhones)
-// document.body.addEventListener('click', () => {
-//   startCamera();
-//   document.querySelector('.instructions').textContent = "Align yourself with the outfit 👗";
-// }, { once: true });
+// Wait for user interaction (needed on iPhones)
+document.body.addEventListener('click', () => {
+  startCamera();
+  document.querySelector('.instructions').textContent = "Align yourself with the outfit 👗";
+}, { once: true });
 
